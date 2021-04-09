b9613273574b1cd67f6b3c78a7b55162e51b4485
brunojamelli <brunojamelli@ufrn.edu.br>
2021-04-09 00:00:22 -0300

    testing server file and first requisitions

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+// const UserController = require('/Controllers/UserController');
+const UserController = require('./controllers/UserController');
+const express = require('express');
+
+const routes = express.Router();
+
+routes.post('/usuario', UserController.post);
+routes.put('/usuario/:id', UserController.put);
+routes.delete('/usuario/:id', UserController.delete);
+routes.get('/usuarios', UserController.get);
+routes.get('/usuario/:id', UserController.getById);
+
+
+module.exports = routes;
