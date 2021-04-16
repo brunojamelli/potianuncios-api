c4b0049e085d8bfa97379a9e8e00b22729d12c77
brunojamelli <brunojamelli@ufrn.edu.br>
2021-04-15 21:43:56 -0300

    adding creation of advertiser #6

--- src/controllers/AdvertiserController.js
+++ src/controllers/AdvertiserController.js
@@ -2,7 +2,45 @@ function generateAccessToken(user) {
     // expires after half and hour (600 seconds = 10 minutes)
     return jwt.sign({ user }, process.env.SECRET, { expiresIn: 600 });
 }
+module.exports = {
+    async create(request, response) {
+        const {
+            name,
+            whatsapp,
+            email,
+            password,
+            address
+        } = request.body;
 
+        if (password == undefined || password.length < 8 || password == '') {
+            return response.status(400).send("Passoword invalid");
+        }
+
+        const id = generateUniqueId();
+        let res = await connection('advertisers').insert({
+            name,
+            whatsapp,
+            email,
+            password,
+            address
+        });
+
+        return response.json({ res });
+    },
+    async login(request, response) {
+        const username = request.body.username;
+        const pwd = request.body.password;
+        // users hardcoded for simplicity, store in a db for production applications
+        const users = [
+            { id: 1, username: 'admin', password: 'admin', firstName: 'Admin', lastName: 'User', role: Role.Admin },
+            { id: 2, username: 'user', password: 'user', firstName: 'Normal', lastName: 'User', role: Role.User }
+        ];
+        const user = users.find(u => u.username === username && u.password === pwd);
+
+        if (!user) { return response.status(500).json({ auth: false, message: 'Login inválido!' }); }
+        return response.json({ auth: true, token: generateAccessToken(user.id), user: { id: user.id, username: user.username, role: user.role } });
+    },
+}
 exports.post = (req, res, next) => {
     res.status(201).send('Rota POST!');
 };
@@ -26,18 +64,6 @@ exports.getById = (req, res, next) => {
     res.status(200).send(`Rota GET com ID! ${id}`);
 };
 
-exports.login = (request, response, next) => {
-    const username = request.body.username;
-    const pwd = request.body.password;
-    // users hardcoded for simplicity, store in a db for production applications
-    const users = [
-        { id: 1, username: 'admin', password: 'admin', firstName: 'Admin', lastName: 'User', role: Role.Admin },
-        { id: 2, username: 'user', password: 'user', firstName: 'Normal', lastName: 'User', role: Role.User }
-    ];
-    const user = users.find(u => u.username === username && u.password === pwd);
 
-    if (!user) { return response.status(500).json({ auth: false, message: 'Login inválido!' }); }
-    return response.json({ auth: true, token: generateAccessToken(user.id), user: { id: user.id, username: user.username, role: user.role } });
-};
 
 
--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,11 @@ const express = require('express');
 
 const routes = express.Router();
 
-routes.post('/Advertiser', AdController.post);
-routes.put('/Advertiser/:id', AdController.put);
-routes.delete('/Advertiser/:id', AdController.delete);
-routes.get('/Advertiser', AdController.get);
-routes.get('/Advertiser/:id', AdController.getById);
+routes.post('/advertiser', AdController.create);
+// routes.put('/Advertiser/:id', AdController.put);
+// routes.delete('/Advertiser/:id', AdController.delete);
+// routes.get('/Advertiser', AdController.get);
+// routes.get('/Advertiser/:id', AdController.getById);
 
 routes.post('/announcement', AnController.create);
 routes.get('/announcement', AnController.show);
