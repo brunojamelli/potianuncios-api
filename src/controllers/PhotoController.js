79114a92f64535493b75fe2e280f83f1780a98e7
brunojamelli <brunojamelli@ufrn.edu.br>
2021-04-20 10:34:57 -0300

    saving photos related to an ad #25

--- src/controllers/PhotoController.js
+++ src/controllers/PhotoController.js
@@ -1,12 +1,40 @@
 const path = require('path');
+const db = require('../database/connection');
 
 module.exports = {
     async create(request, response) {
+        const { an_id } = request.body;
+        var filename, filepath, mimetype, size;
         request.files.forEach(element => {
-            console.log(element.filename);
+            filename = element.filename;
+            filepath = element.filename;
+            mimetype = element.mimetype;
+            size = element.size;
+            db
+                .insert({
+                    filename,
+                    filepath,
+                    mimetype,
+                    size,
+                    an_id
+                })
+                .into('photos')
+                .then(() => console.log({ success: true, filename }))
+                .catch(err => console.log(
+                    {
+                        success: false,
+                        message: 'upload failed',
+                        stack: err.stack,
+                    }
+                )
+                );
         });
-        return response.json('/image api');
-        // return response.status(201).send({result: "success"});
+
+
+
+
+        // return response.json('/image api');
+        return response.status(201).send({ result: "success" });
     },
     async show(request, response) {
         const { filename } = request.params;
