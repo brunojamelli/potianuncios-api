8ea0a8f15388a91e423c72f2e7e241be72fd6340
brunojamelli <brunojamelli@ufrn.edu.br>
2021-05-01 09:12:38 -0300

    eliminating unused commented code

--- src/routes.js
+++ src/routes.js
@@ -14,13 +14,6 @@ var storage = multer.diskStorage({
         console.log(file)
         callback(null, `${Date.now()} - ${file.originalname}`);
     },
-    // fileFilter: function (req, file, callback) {
-    //     var ext = path.extname(file.originalname);
-    //     if (ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg') {
-    //         return callback(new Error('Only images are allowed'))
-    //     }
-    //     callback(null, true)
-    // },
     limits: {
         fileSize: 1024 * 1024
     }
@@ -41,7 +34,6 @@ routes.post('/advertiser', celebrate({
     })
 }), AdController.create);
 routes.put('/Advertiser/:id', AdController.edit);
-// routes.delete('/Advertiser/:id', AdController.delete);
 routes.get('/advertiser/:id', AdController.profile);
 routes.get('/Advertiser', AdController.index);
 
