4638a1e7aae8928ddff04158c59fe03e93ae6ec0
brunojamelli <brunojamelli@ufrn.edu.br>
2021-07-27 22:29:40 -0300

    change knex config

--- knexfile.js
+++ knexfile.js
@@ -50,7 +50,10 @@ module.exports = {
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: __dirname + '/src/database/migrations',
+    },
+    seeds: {
+      directory: __dirname + '/src/database/seeds'
     }
   }
 
