07393fcf5d9befddf9f3ad6d2af7dad26df879d6
brunojamelli <brunojamelli@ufrn.edu.br>
2021-07-28 10:00:33 -0300

    change knex config

--- knexfile.js
+++ knexfile.js
@@ -40,7 +40,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL+'?sslmode=require',
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
