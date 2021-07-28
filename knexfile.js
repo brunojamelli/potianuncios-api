2339e76eee0a6ef9c9a749475466e60281271821
brunojamelli <brunojamelli@ufrn.edu.br>
2021-07-28 09:51:16 -0300

    ssl config

--- knexfile.js
+++ knexfile.js
@@ -40,7 +40,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL+'sslmode=require',
+    connection: process.env.DATABASE_URL+'?sslmode=require',
     pool: {
       min: 2,
       max: 10
