7d45a12008bb63b8023c86f4cdbc6df962aed980
brunojamelli <brunojamelli@ufrn.edu.br>
2021-08-27 20:03:45 -0300

    logger usage in autorization middleware #62

--- src/helpers/authMiddleware.js
+++ src/helpers/authMiddleware.js
@@ -1,16 +1,17 @@
 const jwt = require('jsonwebtoken');
+const { logger } = require("../logger");
 
 const roleController = (permissions) => {
     return (req, res, next) => {
         const token = req.headers.authorization;
         let decoded_token, user_role;
-        console.log(`encoded token - ${token}`);
+        logger.debug(`encoded token - ${token}`)
         jwt.verify(token, process.env.SECRET, function (err, decoded) {
             decoded_token = decoded
         });
         user_role = decoded_token.user.role;
         if (permissions.includes(user_role)) {
-            console.log(`decoded token role info - ${decoded_token.user.role}`);
+            logger.debug(`user logged role info - ${decoded_token.user.role}`);
             next();
         } else {
             return res.status(401).json("You dont have permission");
